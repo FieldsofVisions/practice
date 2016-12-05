8898242aa042b24ae0cface618de22e52dfe4770
Andre Fields <aundrey800@gmail.com>
2016-12-04 17:25:29 -0800

    comitted

--- entry.js
+++ entry.js
@@ -3,68 +3,31 @@ import ReactDOM from 'react-dom';
 require('!style!css!sass!./style.scss');
 
 class CoolStuff extends React.Component{
-	constructor(){
-		super()
-		this.state={editing:false};
-		
-	}
-	edit(){
-		this.setState({editing:true});
-	}
-	remove(){
-		comments.slice(1,i);
-	}
-	save(){
-		this.setState({editing: false})
-		var val = this.refs.newText.value;
-	}
 		
 	render(){
-		if(this.state.editing){
-			
-			return(<div  className="board">
-		      <textarea ref= "newText" defaultValue={this.props.children}></textarea>
-           <button className="btn-save" onClick={this.save.bind(this)}>SAVE</button>		   
-		</div>
-		);
-		
-		}else{
-			
-			return(<div className="board">
-			   <h2>{this.props.children}</h2>
-                 <button onClick={this.edit.bind(this)} className="btn-primary">EDIT</button>
-	           <button onClick={this.remove.bind(this)} className="btn-warn">REMOVE</button>
-			</div>	
-		);	
-		}
+		let recipeItems = this.props.recipes.map((recipe, i)=>{
+			return <li key={i}>{recipe}</li>
+		});
+		return(<div>{recipeItems}</div>)
 		  
 	}
 }
 
 class Board extends React.Component{
-	constructor(){
-		super()
-		this.state={comments: [
-		'andre', 
-		'is',
-		'awesome'
-		]}
-	}
+	
 	render(){
+		let name = "I'm Fucking awesome!!!"
+		let recipes = ["chicken","pork","Steak","Fish"]
 		return(
-		<div className="bigBoard">
-		{
-			this.state.comments.map(function(item, i){
-				return(<CoolStuff key={i}>{item}</CoolStuff>)
-			})
-			
-		}
+		<div >
+		Hello World! {name}
+		<CoolStuff recipes={recipes} />
        </div>
 		);
 	}
 }
 
 ReactDOM.render(
- <Board />,
+ <Board  name= "I'm fucking awesome" />,
 document.getElementById('stuff')
 );
